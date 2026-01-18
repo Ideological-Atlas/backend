1ae4ab6a8d71cc2c0dce155a3692324af6349523
martingaldeca <martingaldeca@gmail.com>
2026-01-18 13:52:26 +0100

    * Add extra tests

--- src/tests/apps/ideology/api/serializers/test_ideology_section_serializers.py
+++ src/tests/apps/ideology/api/serializers/test_ideology_section_serializers.py
@@ -1,8 +1,17 @@
 from unittest.mock import MagicMock
 
 from core.api.api_test_helpers import SerializerTestBase
-from ideology.api.serializers import IdeologySectionSerializer
-from ideology.factories import IdeologySectionFactory
+from ideology.api.serializers import (
+    IdeologySectionConditionerSerializer,
+    IdeologySectionSerializer,
+)
+from ideology.factories import (
+    IdeologyAxisFactory,
+    IdeologyConditionerFactory,
+    IdeologySectionConditionerFactory,
+    IdeologySectionFactory,
+)
+from ideology.models import IdeologyConditioner
 
 
 class IdeologySectionSerializerTestCase(SerializerTestBase):
@@ -24,3 +33,31 @@ class IdeologySectionSerializerTestCase(SerializerTestBase):
 
                 serializer = IdeologySectionSerializer(section)
                 self.assertEqual(serializer.data["icon"], expected)
+
+
+class IdeologySectionConditionerSerializerTestCase(SerializerTestBase):
+    def test_axis_range_conditioner_removes_condition_values(self):
+        axis = IdeologyAxisFactory()
+        conditioner = IdeologyConditionerFactory(
+            type=IdeologyConditioner.ConditionerType.AXIS_RANGE,
+            source_axis=axis,
+            axis_min_value=50,
+        )
+        rule = IdeologySectionConditionerFactory(conditioner=conditioner)
+        serializer = IdeologySectionConditionerSerializer(rule)
+        data = serializer.data
+        self.assertNotIn("condition_values", data)
+        self.assertEqual(data["conditioner"]["type"], "axis_range")
+
+    def test_categorical_conditioner_keeps_condition_values(self):
+        conditioner = IdeologyConditionerFactory(
+            type=IdeologyConditioner.ConditionerType.CATEGORICAL,
+            accepted_values=["A", "B"],
+        )
+        rule = IdeologySectionConditionerFactory(
+            conditioner=conditioner, condition_values=["A"]
+        )
+        serializer = IdeologySectionConditionerSerializer(rule)
+        data = serializer.data
+        self.assertIn("condition_values", data)
+        self.assertEqual(data["condition_values"], ["A"])
