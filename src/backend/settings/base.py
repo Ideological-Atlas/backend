b16a7f8fbeebcb3a6ebcfec55020e6c91d15030d
martingaldeca <martingaldeca@gmail.com>
2026-02-21 11:08:06 +0100

    * Fix connections issue

--- src/backend/settings/base.py
+++ src/backend/settings/base.py
@@ -131,7 +131,7 @@ DATABASES = {
         "PASSWORD": env("POSTGRES_PASSWORD", default=""),
         "HOST": env("POSTGRES_HOST", default="postgres"),
         "PORT": env("POSTGRES_PORT", default=""),
-        "CONN_MAX_AGE": env.int("CONN_MAX_AGE", default=1000),
+        "CONN_MAX_AGE": env.int("CONN_MAX_AGE", default=0),
         "DISABLE_SERVER_SIDE_CURSORS": True,
         "TEST": {
             "NAME": "testing_database",
